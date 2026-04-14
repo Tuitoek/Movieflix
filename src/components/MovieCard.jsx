44c1420c68638e21f3a174f028cf7849a95e015f
Tuitoek <sjtoek@gmail.com>
2026-04-14 15:18:25 +0300

    Add image path to movie card from api

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+
 const MovieCard = ({ movie: { id, title, vote_average, 
     poster_path, release_date, original_language } }) => {
-        
+
   return (
-    <div>
+    <div className='movie-card'>
       <p key={id} className='text-white'>{title}</p>
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png' } alt={title} />
     </div>
   );
 };
