301c33bb321e1957481d9ed9a451ce5c9fc9d407
Tuitoek <sjtoek@gmail.com>
2026-04-17 15:25:27 +0300

    Create Client on Supabase

--- src/Supabase.js
+++ src/Supabase.js
@@ -1,10 +1,12 @@
 const DATABASE_ID = import.meta.env.VITE_SUPABASE_URL;
 const ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+const client = new SupabaseClient(DATABASE_ID, ANON_KEY).setEndpoint(DATABASE_ID).setKey(ANON_KEY);
+
 export const updateSearchCount = async (searchTerm, movie) => {
     // Use supabase API to check if the search term already exists in the database
 
     //If it does update the count, if not create a new entry with count 1
-    
+
     // If it doesn't, create a new entry with count 1
 }
